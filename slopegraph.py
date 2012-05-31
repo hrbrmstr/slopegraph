263a062dbbf15fe031e0101b08a7211cb758401a
Bob Rudis <bob@rudis.net>
2012-05-31 15:12:15 -0400

    0.7.1 - general tweaks & fixes & cleanup + test data add/mod
    new sample data file to play with;
    new theme sample;
    formatting tweaks & fixes;
    added hash bang;
    moved font family to variable in prep for refactor

--- slopegraph.py
+++ slopegraph.py
@@ -1,65 +1,75 @@
+#!/usr/bin/python 
+#
 # slopegraph.py
 #
 # Author: Bob Rudis (@hrbrmstr)
 #
 # Basic Python skeleton to do simple two value slopegraphs
-# with output to PDF (most useful form for me...Cairo has tons of options)
 #
-# Find out more about & download Cairo here:
-# http://cairographics.org/
+# Find out more about & download Cairo here: http://cairographics.org/
 #
-# 2012-05-28 - 0.5 - Initial github release. Still needs some polish
-# 2012-05-29 - 0.6 - Value labels now align; Added object colors; Changed example to use serif
-# 2012-05-30 - 0.7 - Corrected slope start/end points; calculates label widths accurately now
-#                    Also tossed in some data set "anomalies" for testing purposes
+# 2012-05-28 - 0.5.0 - Initial github release. Still needs some polish
+# 2012-05-29 - 0.6.0 - Value labels now align; Added object colors; Changed example to use serif
+# 2012-05-30 - 0.7.0 - Corrected slope start/end points; calculates label widths accurately now
+#                      Also tossed in some data set "anomalies" for testing purposes
+# 2012-05-31 - 0.7.1 - New sample data file to play with; new theme sample; formatting tweaks & fixes
+#                      Added hashbang; moved font family to variable in prep for refactor
 #
 
 import csv
 import cairo
 
 # original data source: http://www.calvin.edu/~stob/data/television.csv
+# (use fields 0, 5 & 4)
+
+# updated data source
+# Median Age of Women at First Marriage, by Age Group - Rankings
+# DataFinder by The Population Reference Bureau: http://bit.ly/KANOPK
+# (use fields 0, 1 & 2)
 
 # get a CSV file to work with 
 
 slopeReader = csv.reader(open('television.csv', 'rb'), delimiter=',', quotechar='"')
+#slopeReader = csv.reader(open('median-age.csv', 'rb'), delimiter=',', quotechar='"')
 
 starts = {} # starting "points"/
 ends = {} # ending "points"
 
-# setup line width and font-size for the Cairo
-# you can change these and the constants should
-# scale the plots accordingly
+# setup line width and font-size for the Cairo you can change these and the constants should
+# scale the plots accordingly. You shld be able to use any system font with Cairo's toy
+# font engine.
 
+FONT_FAMILY = "Palatino"
 FONT_SIZE = 20
 LINE_WIDTH = 0.5
 
-# setup some more constants for plotting
-# all of these are malleable and should cascade nicely
+# setup some more constants for plotting all of these are malleable and should cascade nicely
 
 X_MARGIN = 20
 Y_MARGIN = 30
 SLOPEGRAPH_CANVAS_SIZE = 300
-spaceWidth = FONT_SIZE / 2.0
+SPACE_WIDTH = FONT_SIZE / 4.0
 LINE_HEIGHT = FONT_SIZE + (FONT_SIZE / 2.0)
 PLOT_LINE_WIDTH = 0.5
+LINE_START_DELTA = 1.5*SPACE_WIDTH
 
 # colors
 
-LAB_R = (140.0/255.0)
-LAB_G = (31.0/255.0)
-LAB_B = (40.0/255.0)
+LAB_R = (34.0/255.0)
+LAB_G = (17.0/255.0)
+LAB_B = (17.0/255.0)
 
-VAL_R = (198.0/255.0)
-VAL_G = (107.0/255.0)
-VAL_B = (26.0/255.0)
+VAL_R = (119.0/255.0)
+VAL_G = (85.0/255.0)
+VAL_B = (85.0/255.0)
 
-LINE_R = (198.0/255.0)
-LINE_G = (182.0/255.0)
-LINE_B = (180.0/255.0)
+LINE_R = (170.0/255.0)
+LINE_G = (119.0/255.0)
+LINE_B = (36.0/255.0)
 
-BG_R = (254.0/255.0)
-BG_G = (249.0/255.0)
-BG_B = (229.0/255.0)
+BG_R = (255.0/255.0)
+BG_G = (255.0/255.0)
+BG_B = (255.0/255.0)
 
 # build a base pair array for the final plotting
 
@@ -67,14 +77,11 @@ pairs = []
 
 for row in slopeReader:
 
-	# add chosen values (need start/end for each CSV row)
-	# to the final plotting array. Try this sample with 
-	# row[1] (average life span) instead of row[5] to see some
-	# of the scaling in action
+	# add chosen values (need start/end for each CSV row) to the final plotting array.
 	
 	lab = row[0] # label
-	beg = float(row[5]) # male life span
-	end = float(row[4]) # female life span
+	beg = float(row[5]) 
+	end = float(row[4]) 
 	
 	pairs.append( (float(beg), float(end)) )
 
@@ -130,8 +137,8 @@ delta = float(delta)
 lowest = float(lowest)
 highest = float(highest)
 
-sWidth = 0
-eWidth = 0
+sWidth = 0.0
+eWidth = 0.0
 
 # there has to be a better way to get a base "surface"
 # to do font calculations besides this. we're just making
@@ -146,7 +153,7 @@ filename = 'slopegraph.pdf'
 surface = cairo.PDFSurface (filename, 8.5*72, 11*72)
 cr = cairo.Context (surface)
 cr.save()
-cr.select_font_face("Serif", cairo.FONT_SLANT_NORMAL, cairo.FONT_WEIGHT_NORMAL)
+cr.select_font_face(FONT_FAMILY, cairo.FONT_SLANT_NORMAL, cairo.FONT_WEIGHT_NORMAL)
 cr.set_font_size(FONT_SIZE)
 cr.set_line_width(LINE_WIDTH)
 
@@ -182,8 +189,8 @@ cr.restore()
 cr.show_page()
 surface.finish()
 
-width = X_MARGIN + sWidth + spaceWidth + startMaxLabelWidth + spaceWidth + SLOPEGRAPH_CANVAS_SIZE + spaceWidth + endMaxLabelWidth + spaceWidth + eWidth + X_MARGIN ;
-height = (Y_MARGIN * 2) + (((highest - lowest + 1) / delta) * LINE_HEIGHT)
+width = X_MARGIN + sWidth + SPACE_WIDTH + startMaxLabelWidth + SPACE_WIDTH + SLOPEGRAPH_CANVAS_SIZE + SPACE_WIDTH + endMaxLabelWidth + SPACE_WIDTH + eWidth + X_MARGIN ;
+height = (Y_MARGIN * 2) + (((highest - lowest) / delta) * LINE_HEIGHT)
 
 # create the real Cairo surface/canvas
 
@@ -196,7 +203,7 @@ cr = cairo.Context (surface)
 
 cr.save()
 
-cr.select_font_face("Serif", cairo.FONT_SLANT_NORMAL, cairo.FONT_WEIGHT_NORMAL)
+cr.select_font_face(FONT_FAMILY, cairo.FONT_SLANT_NORMAL, cairo.FONT_WEIGHT_NORMAL)
 cr.set_font_size(FONT_SIZE)
 
 cr.set_line_width(LINE_WIDTH)
@@ -216,11 +223,11 @@ for k in sorted(startKeys):
 	val = float(k)
 
 	cr.set_source_rgb(LAB_R,LAB_G,LAB_B)
-	cr.move_to(X_MARGIN + (sWidth - lWidth), Y_MARGIN + (highest - val) * LINE_HEIGHT * (1/delta) + LINE_HEIGHT/2)
+	cr.move_to(X_MARGIN + (sWidth - lWidth), Y_MARGIN + (highest - val) * LINE_HEIGHT * (1/delta))
 	cr.show_text(label)
 	
 	cr.set_source_rgb(VAL_R,VAL_G,VAL_B)
-	cr.move_to(X_MARGIN + sWidth + spaceWidth + (startMaxLabelWidth - kWidth), Y_MARGIN + (highest - val) * LINE_HEIGHT * (1/delta) + LINE_HEIGHT/2)
+	cr.move_to(X_MARGIN + sWidth + SPACE_WIDTH + (startMaxLabelWidth - kWidth), Y_MARGIN + (highest - val) * LINE_HEIGHT * (1/delta))
 	cr.show_text(str(k))
 	
 	cr.stroke()
@@ -235,11 +242,11 @@ for k in sorted(endKeys):
 	val = float(k)
 		
 	cr.set_source_rgb(VAL_R,VAL_G,VAL_B)
-	cr.move_to(width - X_MARGIN - spaceWidth - eWidth - spaceWidth - endMaxLabelWidth, Y_MARGIN + (highest - val) * LINE_HEIGHT * (1/delta) + LINE_HEIGHT/2)
+	cr.move_to(width - X_MARGIN - SPACE_WIDTH - eWidth - SPACE_WIDTH - endMaxLabelWidth, Y_MARGIN + (highest - val) * LINE_HEIGHT * (1/delta))
 	cr.show_text(str(k))
 
 	cr.set_source_rgb(LAB_R,LAB_G,LAB_B)
-	cr.move_to(width - X_MARGIN - spaceWidth - eWidth, Y_MARGIN + (highest - val) * LINE_HEIGHT * (1/delta) + LINE_HEIGHT/2)
+	cr.move_to(width - X_MARGIN - SPACE_WIDTH - eWidth, Y_MARGIN + (highest - val) * LINE_HEIGHT * (1/delta))
 	cr.show_text(label)
 
 	cr.stroke()
@@ -250,11 +257,10 @@ cr.set_line_width(PLOT_LINE_WIDTH)
 cr.set_source_rgb(LINE_R, LINE_G, LINE_B)
 
 for s1,e1 in pairs:
-	cr.move_to(X_MARGIN + sWidth + spaceWidth + startMaxLabelWidth + 1.5*spaceWidth, Y_MARGIN + (highest - s1) * LINE_HEIGHT * (1/delta) + LINE_HEIGHT/2)
-	cr.line_to(width - X_MARGIN - eWidth - spaceWidth - endMaxLabelWidth - 1.5*spaceWidth, Y_MARGIN + (highest - e1) * LINE_HEIGHT * (1/delta) + LINE_HEIGHT/2)
+	cr.move_to(X_MARGIN + sWidth + SPACE_WIDTH + startMaxLabelWidth + LINE_START_DELTA, Y_MARGIN + (highest - s1) * LINE_HEIGHT * (1/delta) - LINE_HEIGHT/4)
+	cr.line_to(width - X_MARGIN - eWidth - SPACE_WIDTH - endMaxLabelWidth - LINE_START_DELTA, Y_MARGIN + (highest - e1) * LINE_HEIGHT * (1/delta) - LINE_HEIGHT/4)
 	cr.stroke()
 
 cr.restore()
 cr.show_page()
 surface.finish()
-
